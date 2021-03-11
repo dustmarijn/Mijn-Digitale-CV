9c6de19cc87df8e0e31a83f9ef4326d743b7c823
Marijn Boeve <marijnboeve@hotmail.com>
2021-03-11 14:35:56 +0100

    Eerste data
    eerste data voor cv, stijl van P moet nog wel worden aangepast

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <link href="/css/style.css" rel="stylesheet">
 
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -19,19 +22,245 @@
 <div class="container">
     <div class="row mt-3">
         <div class="col-md-8">
-            
+            <img src="/img/pf.png" class="p-2" width="20%" height="auto" style=" float: left;">
+            <div>
+                <h1 class="bold">Marijn Boeve</h1>
+                <p class="text-secondary" style="max-width: 50vw;">Hallo! Ik ben Marijn, 18 jaar oud, en heb een liefde
+                    voor programmeren. Ik ben al sinds 2018 bezig met
+                    programmeren en cms’en. Op het moment
+                    volg ik een opleiding in Application Development
+                    die ik hoop snel af te ronden. Daarnaast hou ik me
+                    graag bezig met foto en video grafie. Daarnaast ben
+                    ik graag buiten en heb ik een actieve rol bij de lokale
+                    scouting vereniging in Hattem. </p>
+            </div>
+            <br>
+            <div class="mt-4" style="max-width: 50vw;">
+                <h3 class="bold">Ervaring</h3>
+
+                <div class="mt-3">
+                    <h5>Persgroep Nederland</h5>
+                    <p class="text-muted">Sep ’16 - Heden</p>
+                    <div class="">
+                        <p class="text-muted font-italic" style="margin: 0px; padding: 0px;">
+                            Ik bezorg de ochtendkranten in een deel van Wapenveld. 6 dagen
+                            per week stap ik voor dat het licht schijnt op de fiets en breng ik bijna
+                            alle betaalde kranten rond.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <h5>Uitzonderlijk Design</h5>
+                    <p class="text-muted">Nov ’20 - Heden</p>
+                    <div class="">
+                        <p class="text-muted font-italic" style="margin: 0px; padding: 0px;">
+                            Ik heb een eigen bedrijfje rondom het designen en programmeren van applicaties.
+                            Daarnaast maak ik voor klanten die iets simpels nodig hebben of een klein budget hebben
+                            een mooie Wordpress site.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <h5>Infracom BV</h5>
+                    <p class="text-muted">Aug ’20 - Jan ‘21</p>
+                    <div class="">
+                        <p class="text-muted font-italic" style="margin: 0px; padding: 0px;">
+                            Ik heb tijdens deze stage veel gewerkt met Pascal en veel database
+                            werk verricht. Daarnaast heb ik ook geleerd hoe je het best iets kunt
+                            designen zodat je een soepel werkende applicatie krijgt.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="mt-5" style="max-width: 50vw;">
+                <h3 class="bold">Bijlages</h3>
+                <div>
+                    <a href="/bijlage/oorkonde.pdf">Oorkonde World Skills</a>
+                </div>
+
+                <div>
+                    <a href="/bijlage/diploma.jpeg">Mavo Diploma</a>
+                </div>
+
+                <div>
+                    <a href="/bijlage/cv.png">Cv</a>
+                </div>
+            </div>
         </div>
 
         <div class="col-6 col-md-4">
+            <div class="">
+                <h3 class="bold">Skills</h3>
 
+                <div class="">
+                    <div>
+                        <i class="fab fa-bootstrap fs-4"></i>
+                        Bootstrap
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 90%" aria-valuenow="90"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fas fa-users fs-4"></i>
+                        Scrum (Agile)
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 85%" aria-valuenow="85"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fab fa-laravel fs-4"></i>
+                        Laravel
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fas fa-database fs-4"></i>
+                        SQL
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="45"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fab fa-uncharted fs-4"></i>
+                        Adobe XD
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fab fa-uncharted fs-4"></i>
+                        Adobe Photoshop
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-5">
+                <h3 class="bold">Programmeer 'talen'</h3>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fab fa-html5 fs-4"></i>
+                        HTML
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fab fa-php fs-4"></i>
+                        Php
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 65%" aria-valuenow="65"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fab fa-css3-alt fs-4"></i>
+                        CSS
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fab fa-js fs-4"></i>
+                        Javascript
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 30%" aria-valuenow="30"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fas fa-code fs-4"></i>
+                        Pascal
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 20%" aria-valuenow="20"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+                <div class="mt-3">
+                    <div>
+                        <i class="fab fa-react fs-4"></i>
+                        React-Native
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 15%" aria-valuenow="15"
+                             aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+
+
+                <div class="">
+                    <h3 class="bold mt-5">Social Media</h3>
+                    <div class="fs-3">
+                        <a href="https://www.instagram.com/dustmarijn/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                        <a href="https://www.linkedin.com/in/marijnboeve/" target="_blank"><i
+                                class="fab fa-instagram"></i></a>
+                        <a href="https://wa.me/qr/XH3Q5IF5N24HC1" target="_blank"><i class="fab fa-whatsapp"></i></a>
+                        <a href="https://github.com/dustmarijn/" target="_blank"><i class="fab fa-github"></i></a>
+                    </div>
+                </div>
+
+            </div>
         </div>
+
     </div>
 
 </div>
 
+<footer class="mt-4">
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+</footer>
 
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js"
+        integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"
+        integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG"
+        crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/bf60f9e3e6.js" crossorigin="anonymous"></script>
 </body>
 </html>
