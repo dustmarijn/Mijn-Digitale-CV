c98a79d5993044cc21b3ee8daa2031ea1e0b98f0
Marijn Boeve <marijnboeve@hotmail.com>
2021-09-20 11:39:17 +0200

    html basis verloren items pagina

--- resources/views/verloren.blade.php
+++ resources/views/verloren.blade.php
@@ -1,13 +1,53 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="nl">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Verloren Item</title>
+    <title>MARIJNBOEVE.NL - Foto's/Video's</title>
 </head>
 <body>
-TEST
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand">MARIJNBOEVE.NL</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="http://marijnboeve.nl/">Home <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<div class="container">
+    <div class="mt-3 text-center">
+        <h3>Een voorwerp gevonden?</h3>
+        <p>Bedankt voor het vinden van deze website! Wilt u hieronder invullen werk apparaat u heeft gevonden.</p>
+    </div>
+
+    <div class="row justify-content-center">
+        <form>
+            <select class="form-select" aria-label="Default select example" required>
+                <option selected>Welk apparaat heeft u gevonden?</option>
+                <option value="1">Laptop</option>
+                <option value="2">Muis</option>
+            </select>
+        </form>
+    </div>
+</div>
+
 </body>
 </html>
