4435a97831f07959d60cd60575c34b8eb7c040af
Marijn Boeve <marijnboeve@hotmail.com>
2021-09-27 11:01:28 +0200

    basic 404 pagina

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <link href="/css/style.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404</title>
+</head>
+<body>
+<div class="container">
+    <center>
+        <h2 class="mt-4">404</h2>
+    </center>
+</div>
+</body>
+</html>
